e871c096ebd732f47a6e917651f1f61a65c7b659
Md.Hasanat Zamil <zamil_surfserver@yahoo.com>
2023-08-24 22:29:59 +0600

    update page

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -62,15 +62,7 @@ export default async function Blog({ params }) {
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
                         viewBox="0 0 14 10"
-                      >
-                        <path
-                          stroke="currentColor"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          d="M1 5h12m0 0L9 1m4 4L9 9"
-                        />
-                      </svg>
+                      ></svg>
                     </Link>
                   </div>
                 </div>
